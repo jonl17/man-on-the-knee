e4b7054b6b8c28a07e3c23db4f01a768b79006b4
Nonni Kosmos <jon@juni.is>
2023-01-24 21:14:58 +0000

    move it

--- src/routes/smoke/+page.svelte
+++ src/routes/smoke/+page.svelte
@@ -1,5 +1,5 @@
 <section class="h-screen w-full relative">
 	<video class="w-full max-h-full absolute bottom-0 left-0" autoplay muted loop>
-		<source src="/videos/smoke.mp4" />
+		<source src="/videos/smoke-transparent.mov" />
 	</video>
 </section>
